1e963bcff316a669fe74ccf54d7388f8e32fb3d1
Shay Mualem <Shaymu01@gmail.com>
2017-12-23 00:00:52 +0200

    load courses list on start using a mockApi

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -8,22 +8,7 @@ class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
 
-    this.state = {
-      course: { title: "" }
-    };
-
-    this.onTitleChanged = this.onTitleChanged.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
-  }
-
-  onTitleChanged(event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({ course: course });
-  }
-
-  onClickSave() {
-    this.props.actions.createCourse(this.state.course);
+    this.state = { };
   }
 
   courseRow(course, index) {
@@ -36,17 +21,6 @@ class CoursesPage extends React.Component {
     return (
       <div>
         <h1>Courses</h1>
-        <h2>Add Course</h2>
-        <input
-          type="text"
-          onChange={this.onTitleChanged}
-          value={this.state.course.title} />
-        <input
-          type="submit"
-          value="Save"
-          onClick={this.onClickSave} />
-
-        <h2>Courses List</h2>
         {this.props.courses.map(this.courseRow)}
       </div>
     );
